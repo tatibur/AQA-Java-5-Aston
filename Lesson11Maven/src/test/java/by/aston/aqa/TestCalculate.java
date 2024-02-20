63db632cc91fe0e9bd25f78c722629093b040aee
t.burakovskaya <t.burakouskaya@gmail.com>
2024-02-20 13:33:53 +0300

    Create Maven project (optimization 2)

--- Lesson11Maven/src/test/java/by/aston/aqa/TestCalculate.java
+++ Lesson11Maven/src/test/java/by/aston/aqa/TestCalculate.java
@@ -2,13 +2,12 @@
 Добавьте зависимости на такие инструменты как JUnit 5, Selenium. */
 package by.aston.aqa;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TestCalculate {
     @Test
     void Sum() {
-       assertEquals(5.5, Calculate.Add(3.5, 2.0));
+        Assertions.assertEquals(5.5, Calculate.Add(3.5, 2.0));
     }
 }
